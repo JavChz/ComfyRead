cf672c131aa80a0efa1308f2d979c159f2377bf5
Javier <13250549+JavChz@users.noreply.github.com>
2023-04-18 03:58:16 -0600

    Initial push

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <div className="textarea-container">
+        <textarea placeholder="Write something..."></textarea>
+      </div>
+    </div>
+  );
+}
+
+export default App;
