40c22d430a78f7d436ecd0045b6819195ceb475d
Javier <13250549+JavChz@users.noreply.github.com>
2025-04-06 20:51:54 -0600

    update styling and configuration for improved theme support and code formatting

--- src/components/Textbox.tsx
+++ src/components/Textbox.tsx
@@ -1,9 +1,9 @@
 const Textbox = ({ theme }: { theme: string }) => {
-	return (
-		<div className={`textarea-container ${theme}`}>
-			<textarea spellCheck="false" placeholder="Just write..." />;
-		</div>
-	);
+  return (
+    <div className={`textarea-container ${theme}`}>
+      <textarea spellCheck="false" placeholder="Just write..." />
+    </div>
+  );
 };
 
 export default Textbox;
